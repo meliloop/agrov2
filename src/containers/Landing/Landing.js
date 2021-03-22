ab0b066293d75b57593b112f666b7d9202b2c599
eanhawk <eanhawk@gmail.com>
2021-03-22 10:29:45 -0300

    landing

--- src/containers/Landing/Landing.js
+++ src/containers/Landing/Landing.js
@@ -10,16 +10,16 @@ const Landing = () => {
     return (
         <Aux>
             <div className="landing--container">
-              <div class="buttons--cont">
+              <div className="buttons--cont">
                   <div className="logo__image">
                       <Logo />
                   </div>
 
-                  <div class="row"><Link to={'/login'} className="button account-btn">Iniciar</Link></div>
-                  <div class="row"><Link to={'/registracion'} className="button button--line ml-auto">Registrarse</Link></div>
-                  <div class="row">{localStorage.getItem('statusPrompt') === 'accepted' || <Link to={'#'} className="button button--line buttonInstall">Instalar</Link>}</div>
+                  <div className="row"><Link to={'/login'} className="button account-btn">Iniciar</Link></div>
+                  <div className="row"><Link to={'/registracion'} className="button button--line ml-auto">Registrarse</Link></div>
+                  <div className="row">{localStorage.getItem('statusPrompt') === 'accepted' || <Link to={'#'} className="button button--line buttonInstall">Instalar</Link>}</div>
               </div>
-              <div class="links--cont">
+              <div className="links--cont">
                   <Link to={'/terminos'} className="link">Términos y condiciones</Link>
                   <Link to={'/politicas'} className="Link">Políticas de privacidad</Link>
               </div>
