8d0e85bb1dce4e1a14e44d28937e4ca20b43cbfc
Meli <casla86@gmail.com>
2021-05-29 12:33:09 -0300

    push

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -145,7 +145,7 @@ export const auth = (email, password) => {
             localStorage.setItem('token', response.data.token);
             localStorage.setItem('expirationDate', expirationDate);
             localStorage.setItem('userId', response.data.user_id);
-               
+            window.webpushr('attributes', {"User ID" : response.data.user_id,  "E-mail" : email});
             dispatch(authSuccess(response.data.token, response.data.user_id));
          })
          .catch(err => {
